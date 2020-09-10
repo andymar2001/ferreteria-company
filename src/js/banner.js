5fa292bee29821cd8941f12b80f1b5a65972b6b4
Andy Mariños <andy_mar_rey@outlook.com>
2020-09-10 10:50:53 -0500

    avanze banner desktop

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,7 +1,6 @@
-$('.banner__slider').slick({
+$(".banner__slider").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2500,
-  arrows:false 
 });
--- src/js/gallery.js
+++
@@ -1,7 +0,0 @@
-$(".gallery-layout__slider").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2500,
-  arrows: false,
-});
